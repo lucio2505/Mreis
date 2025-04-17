8a73113bb60c335a204fc62a820337458f87a01a
lucio2505 <lucio.j2505@gmail.com>
2025-04-17 18:47:50 -0300

    estilizando pagina servico

--- js/servico.js
+++ js/servico.js
@@ -20,3 +20,63 @@ window.addEventListener('scroll', checkMovementOnScroll);
 
 // Chama a função ao carregar a página para elementos que já estão visíveis
 checkMovementOnScroll();
+
+
+//CARROSSEL//
+const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+
+let currentSlide = 0;
+
+function hideSlider(){
+    slider.forEach(item => item.classList.remove('on'))
+}
+
+function showSlider(){
+    slider[currentSlide].classList.add('on')
+}
+
+function nextSlider() {
+    hideSlider()
+    if(currentSlide == slider.length -1) {
+        currentSlide = 0
+    }else{
+        currentSlide++
+    }
+    showSlider()
+}
+
+function prevSlider() {
+    hideSlider()
+    if(currentSlide == 0) {
+        currentSlide = slider.length -1
+    }else{
+        currentSlide--
+    }
+    showSlider()
+}
+
+btnNext.addEventListener('click', nextSlider)
+btnPrev.addEventListener('click', prevSlider)
+
+setInterval(nextSlider, 6000);
+
+console.log(slider)
+
+const moveElementsslider = document.querySelectorAll('.move-on-scroll');
+
+function moveOnScroll() {
+    moveElementsslider.forEach((element) => {
+        const position = element.getBoundingClientRect().top;
+        const screenPosition = window.innerHeight / 1.2;
+
+        if (position < screenPosition) {
+            element.classList.add('move');
+        }
+    });
+}
+
+window.addEventListener('scroll', moveOnScroll);
+
+moveOnScroll();
